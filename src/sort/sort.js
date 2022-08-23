24527c402e5e8d8ba973eddc6448e3ebe624ecd6
Konstantin <90931861+Kostikbush@users.noreply.github.com>
2022-08-23 18:53:26 +0300

    ///

--- src/sort/sort.js
+++ src/sort/sort.js
@@ -3,4 +3,5 @@ export const ArrSort = (a,b) => {
         if(a.timeCompleted===b.timeCompleted)return 0;
         if(a.timeCompleted<b.timeCompleted)return -1;
         if(a.timeCompleted === null && b.timeCompleted !== null)return -1;
+        
 }
